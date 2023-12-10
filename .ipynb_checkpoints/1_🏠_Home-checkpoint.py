e0a01393b189b281bc3dbbf0eb5fe3867104e1c4
Kshitij Yadav <kshitij@Kshitijs-MacBookAir.local>
2023-12-09 23:21:10 -0500

    adding improvements

--- .ipynb_checkpoints/1_🏠_Home-checkpoint.py
+++ .ipynb_checkpoints/1_🏠_Home-checkpoint.py
@@ -193,123 +193,136 @@ with st.spinner(text="Building line"):
 
 
 # -----------------  endorsement  ----------------- #
-with st.container():
-    # Divide the container into three columns
-    col1,col2,col3 = st.columns([0.475, 0.475, 0.05])
-    # In the first column (col1)        
-    with col1:
-        # Add a subheader to introduce the coworker endorsement slideshow
-        st.subheader("👄 Coworker Endorsements")
-        # Embed an HTML component to display the slideshow
-        components.html(
-        f"""
-        <!DOCTYPE html>
-        <html>
-        <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- Styles for the slideshow -->
-        <style>
-            * {{box-sizing: border-box;}}
-            .mySlides {{display: none;}}
-            img {{vertical-align: middle;}}
+st.subheader("👄 Coworker Endorsements")
+# Embed an HTML component to display the slideshow
+components.html(
+f"""
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Styles for the slideshow -->
+<style>
+    * {{box-sizing: border-box;}}
+    .mySlides {{display: none;}}
+    img {{vertical-align: middle;}}
 
-            /* Slideshow container */
-            .slideshow-container {{
-            position: relative;
-            margin: auto;
-            width: 100%;
-            }}
+    /* Slideshow container */
+    .slideshow-container {{
+    position: relative;
+    margin: auto;
+    width: 100%;
+    }}
 
-            /* The dots/bullets/indicators */
-            .dot {{
-            height: 15px;
-            width: 15px;
-            margin: 0 2px;
-            background-color: #eaeaea;
-            border-radius: 50%;
-            display: inline-block;
-            transition: background-color 0.6s ease;
-            }}
+    /* The dots/bullets/indicators */
+    .dot {{
+    height: 15px;
+    width: 15px;
+    margin: 0 2px;
+    background-color: #eaeaea;
+    border-radius: 50%;
+    display: inline-block;
+    transition: background-color 0.6s ease;
+    }}
 
-            .active {{
-            background-color: #6F6F6F;
-            }}
+    .active {{
+    background-color: #6F6F6F;
+    }}
 
-            /* Fading animation */
-            .fade {{
-            animation-name: fade;
-            animation-duration: 1s;
-            }}
+    /* Fading animation */
+    .fade {{
+    animation-name: fade;
+    animation-duration: 1s;
+    }}
 
-            @keyframes fade {{
-            from {{opacity: .4}} 
-            to {{opacity: 1}}
-            }}
+    @keyframes fade {{
+    from {{opacity: .4}} 
+    to {{opacity: 1}}
+    }}
 
-            /* On smaller screens, decrease text size */
-            @media only screen and (max-width: 300px) {{
-            .text {{font-size: 11px}}
-            }}
-            </style>
-        </head>
-        <body>
-        <div class="slideshow-container">
-            <div class="mySlides fade">
-                <img src="data:image/jpeg;base64,{endorsements['img1']}" style="width:100%">
-            </div>
+    /* On smaller screens, decrease text size */
+    @media only screen and (max-width: 300px) {{
+    .text {{font-size: 11px}}
+    }}
+    </style>
+</head>
+<body>
+<div class="slideshow-container">
+    <div class="mySlides fade">
+        <img src="data:image/jpeg;base64,{endorsements['img1']}" style="width:100%">
+    </div>
 
-            <div class="mySlides fade">
-                <img src="data:image/jpeg;base64,{endorsements['img2']}" style="width:100%">
-            </div>
+    <div class="mySlides fade">
+        <img src="data:image/jpeg;base64,{endorsements['img2']}" style="width:100%">
+    </div>
 
-            <div class="mySlides fade">
-                <img src="data:image/jpeg;base64,{endorsements['img3']}" style="width:100%">
-            </div>
-        </div>
-        <br>
-        <div style="text-align:center">
-            <span class="dot"></span> 
-            <span class="dot"></span> 
-            <span class="dot"></span> 
-        </div>
+    <div class="mySlides fade">
+        <img src="data:image/jpeg;base64,{endorsements['img3']}" style="width:100%">
+    </div>
+</div>
+<br>
+<div style="text-align:center">
+    <span class="dot"></span> 
+    <span class="dot"></span> 
+    <span class="dot"></span> 
+</div>
 
-        <script>
-            var slideIndex = 0;
-            showSlides();
+<script>
+    var slideIndex = 0;
+    showSlides();
 
-            function showSlides() {{
-                var i;
-                var slides = document.getElementsByClassName("mySlides");
-                var dots = document.getElementsByClassName("dot");
-                for (i = 0; i < slides.length; i++) {{
-                    slides[i].style.display = "none";  
-                }}
-                slideIndex++;
-                if (slideIndex > slides.length) {{slideIndex = 1}}    
-                for (i = 0; i < dots.length; i++) {{
-                    dots[i].className = dots[i].className.replace(" active", "");
-                }}
-                slides[slideIndex-1].style.display = "block";  
-                dots[slideIndex-1].className += " active";
-                setTimeout(showSlides, 2500); // Change image every 2.5 seconds
-            }}
-        </script>
-    </body>
-    </html> 
-    """,
-    height=270,
+    function showSlides() {{
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("dot");
+        for (i = 0; i < slides.length; i++) {{
+            slides[i].style.display = "none";  
+        }}
+        slideIndex++;
+        if (slideIndex > slides.length) {{slideIndex = 1}}    
+        for (i = 0; i < dots.length; i++) {{
+            dots[i].className = dots[i].className.replace(" active", "");
+        }}
+        slides[slideIndex-1].style.display = "block";  
+        dots[slideIndex-1].className += " active";
+        setTimeout(showSlides, 2500); // Change image every 2.5 seconds
+    }}
+</script>
+</body>
+</html> 
+""",
+height=270,
 )
 
-# -----------------  contact  ----------------- #
+import streamlit as st
+
+with st.container():
+    col1, col2, col3 = st.columns(3)
+
+    # LinkedIn Icon
+    with col1:
+        st.markdown(
+            """<a href="https://www.linkedin.com/in/nandinicomar/" target="_blank">
+                   <img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn">
+               </a>""",
+            unsafe_allow_html=True
+        )
+
+    # Email Icon
     with col2:
-        st.subheader("📨 Contact Me")
-        contact_form = f"""
-        <form action="https://formsubmit.co/{info["Email"]}" method="POST">
-            <input type="hidden" name="_captcha value="false">
-            <input type="text" name="name" placeholder="Your name" required>
-            <input type="email" name="email" placeholder="Your email" required>
-            <textarea name="message" placeholder="Your message here" required></textarea>
-            <button type="submit">Send</button>
-        </form>
-        """
-        st.markdown(contact_form, unsafe_allow_html=True)
+        st.markdown(
+            """<a href="mailto:comarnan@gmail.com">
+                   <img src="https://img.icons8.com/fluent/48/000000/gmail.png" alt="Email">
+               </a>""",
+            unsafe_allow_html=True
+        )
+
+    # Phone Icon
+    with col3:
+        st.markdown(
+            """<a href="tel:+1.201.620.1544">
+                   <img src="https://img.icons8.com/fluent/48/000000/phone.png" alt="Phone">
+               </a>""",
+            unsafe_allow_html=True
+        )
+
