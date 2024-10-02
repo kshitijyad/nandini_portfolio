485c58cb945bb2bdd4632969adc0b8f16e79c4c7
Kshitij Yadav <kshitijyad@gmail.com>
2024-10-01 22:05:16 -0400

    changing spelling mistake

--- constant.py
+++ constant.py
@@ -2,7 +2,7 @@ info = {
    "Pronoun": "her", 
    "Name": "Nandini",
    "Full_Name": "Nandini Comar",
-   "Intro": "New York Registered Lawyer, with experince in Intellectual Property, Trademark, Contracts and Patents",
+   "Intro": "New York Registered Lawyer, with experience in Intellectual Property, Trademark, Contracts and Patents",
    "About":"Hi, I'm Nandini Comar, an experienced attorney specializing in commercial contracts and technology agreements. With 5+ years of experience, I've negotiated and drafted over 100 contracts, including software, SaaS, data license, and professional services agreements. I excel at developing strategic partnerships with business leaders, implementing best practices, and advising on complex legal issues and risks. My background includes managing IP portfolios for 25+ clients, reducing infringement incidents by 90%, and navigating patent litigation. I'm adept at translating complex legal concepts into clear, actionable insights for non-legal stakeholders, ensuring effective communication across teams. My experience in contract compliance, risk mitigation, and cross-functional collaboration makes me well-suited to support Moody's financial intelligence, data analytics, and software services businesses.",
    "City":"New York, United States",
    "Email": "comarnan@gmail.com"
